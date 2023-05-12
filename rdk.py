0151a8d84159daa0918ba4131dac96961ed88ab5
joneatene <joneatene@gmail.com>
2023-05-12 20:51:08 +0300

    finished code

--- rdk.py
+++ rdk.py
@@ -1,14 +1,14 @@
-from psychopy import visual, event, core, data
+from psychopy import visual, event, core
 import random
 import numpy as np
 
 # Experiment parameters
-n_trials = 5
-n_dots = 800
-coherences = [1, 30, 60, 90]
+n_trials = 25
+n_dots = 300
+coherences = [1, 5, 10, 20, 40, 80]
 length_coherences = len(coherences)
-dot_speed = 10 # in pixels per frame
-dot_size = 5  # in pixels
+dot_speed = 30 # in pixels per frame
+dot_size = 5 # in pixels
 bg_color = 'black'
 fixation_color = 'white'
 dot_color = 'white'
@@ -29,7 +29,8 @@ dots_xys = [(random.uniform(-150, 150), random.uniform(-150, 150)) for i in rang
 dots = visual.ElementArrayStim(win, nElements=n_dots,
                                sizes=dot_size, colors=dot_color, elementTex=None,
                                xys=dots_xys, fieldPos=(0, 0),
-                               fieldSize=(300, 300), fieldShape='Circle')
+                               fieldSize=(300, 300), fieldShape='Square')
+clock = core.Clock()
 
 # Instructions
 instructions = """
@@ -49,14 +50,29 @@ event.waitKeys(keyList=['space'])
 
 correct_responses = 0
 participant_data = []
+
+#Trial loop
 # Trial loop
 for coherence in coherences:
     print(coherence)
     for trial in range(n_trials):
-        dots.direction = random.choice([-1, 1]) * coherence
-        dots_speed = dots.direction * dot_speed
-        dots.xys += dots_speed / frames_per_second
-        dots.xys += np.random.normal(scale=0.01, size=(n_dots, 2))
+        # Set coherence
+        direction = random.choice([-1, 1])
+        coherent_dots = int(n_dots * coherence / 100)
+        direction_dots = [direction] * coherent_dots + [0] * (n_dots - coherent_dots)
+        random.shuffle(direction_dots)
+
+        # Update dot positions
+        for i in range(n_dots):
+            direction_dot = np.deg2rad(90 - i * 360/n_dots)
+            x = dots_xys[i][0]
+            y = dots_xys[i][1]
+            x += dot_speed * np.cos(direction_dot) / frames_per_second
+            y += dot_speed * np.sin(direction_dot) / frames_per_second
+            dots_xys[i] = (x, y)
+
+        # Update ElementArrayStim
+        dots.xys = dots_xys
 
         # Draw stimuli
         dots.draw()
@@ -70,20 +86,14 @@ for coherence in coherences:
         if response == [escape_key]:
             win.close()
             core.quit()
-        elif (dots.direction < 0 and response[0] == 'left') or \
-             (dots.direction > 0 and response[0] == 'right'):
+        elif (direction < 0 and response[0] == 'left') or \
+            (direction > 0 and response[0] == 'right'):
             correct_responses += 1
             participant_data.append("[{}, 1]".format(coherence))
         else:
             participant_data.append("[{}, 0]".format(coherence))
 
-        # Clear the screen
-        win.flip()
-
-        # Pause before the next trial
-        core.wait(stimulus_duration)
-
-
+# Save participant data
 with open('results.txt', 'w') as f:
     f.write(" ".join(participant_data))
 
@@ -93,6 +103,3 @@ print("Participant's accuracy: {}%".format((correct_responses/(n_trials*(len(coh
 # Close the window and quit the program
 win.close()
 core.quit()
-
-
-
